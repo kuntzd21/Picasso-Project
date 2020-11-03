c61ada1a4adfc2763c39cd1b710ab917319f5f60
Jay Choi <choij21@mail.wlu.edu>
2020-11-02 21:10:43 -0500

    Created an expression for addition in Picasso as well as an analyzer for
    plus.

--- src/picasso/parser/PlusAnalyzer.java
+++ src/picasso/parser/PlusAnalyzer.java
@@ -3,7 +3,10 @@ package picasso.parser;
 import java.util.Stack;
 
 import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Addition;
+import picasso.parser.tokens.NumberToken;
 import picasso.parser.tokens.Token;
+import picasso.parser.tokens.operations.PlusToken;
 
 /**
  * Handles parsing the plus or "addition function".
@@ -13,14 +16,39 @@ import picasso.parser.tokens.Token;
  * 
  */
 public class PlusAnalyzer implements SemanticAnalyzerInterface {
+	
+	private static PlusAnalyzer singleton;
+
+	public static PlusAnalyzer getInstance() {
+		if (singleton == null) {
+			singleton = new PlusAnalyzer();
+		}
+		return singleton;
+	}
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Remove the plus token
+		// Remove the plus token
+		PlusToken ptoken = (PlusToken) tokens.pop(); 
+		
+		if (ptoken.isFunction() != true) {
+			throw new ParseException("Not a function.");
+		}
+		
 		// the parameters are the next tokens on the stack.
+		NumberToken ntoken = (NumberToken) tokens.pop();
+		
 		// But, they need to be processed
+		double value = ntoken.value();
+		
+		if (value < -1 || value > 1) {
+			throw new ParseException("Constant is out of range [-1, 1]");
+		}
+		
 		// TODO: Need to finish.
-		return null;
+		return new Addition(SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens), SemanticAnalyzer.getInstance().generateExpressionTree(
+						tokens) );
 	}
 
 }
