164347ab99da1be60afff0d6eb7812ce9db6061f
Jay Choi <choij21@mail.wlu.edu>
2020-11-13 17:07:05 -0500

    Re-added the binary operator exponentiate.

---
+++ src/picasso/parser/ExponentiateAnalyzer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package picasso.parser;
+
+/**
+ * @author jaychoi
+ *
+ */
+public class ExponentiateAnalyzer {
+
+	/**
+	 * 
+	 */
+	public ExponentiateAnalyzer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/picasso/parser/language/expressions/Exponentiate.java
@@ -0,0 +1,39 @@
+
+/**
+ * 
+ */
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the exponentiate function in the Picasso language.
+ * 
+ * @author Linkimals
+ *
+ */
+public class Exponentiate extends BinaryOperator {
+
+	public Exponentiate(ExpressionTreeNode left, ExpressionTreeNode right) {
+		super(left, right);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the exponentiation of
+	 * the function's parameters.
+	 * 
+	 * @return the color from evaluating the exponentiation of the expression's parameters
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = left.evaluate(x, y);
+		RGBColor result2 = right.evaluate(x, y);
+		
+		double red = Math.pow(result.getRed(), result2.getRed());
+		double green = Math.pow(result.getGreen(), result2.getGreen());
+		double blue = Math.pow(result.getBlue(), result2.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+	
+}
