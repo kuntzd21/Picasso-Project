b1f72f8c4ffd229cf3068ada90c75a8f4d659ff4
Nick Steinert <nicholassteinert@nicholass-mbp.ad.wlu.edu>
2020-11-13 16:27:09 -0500

    added back cosine token

---
+++ src/picasso/parser/tokens/functions/CosToken.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the sine function token.
+ *
+ */
+public class CosToken extends FunctionToken {
+	
+	public CosToken() {
+		super("Cosine Function Token");
+	}
+
+}
