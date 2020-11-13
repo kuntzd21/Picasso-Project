783e313a7e23c697adf22a6f7a129ac843fcf703
Jay Choi <choij21@mail.wlu.edu>
2020-11-13 17:07:24 -0500

    Re-added the binary operator exponentiate.

--- src/picasso/parser/ExponentiateAnalyzer.java
+++ src/picasso/parser/ExponentiateAnalyzer.java
@@ -3,17 +3,41 @@
  */
 package picasso.parser;
 
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Exponentiate;
+import picasso.parser.tokens.Token;
+
 /**
- * @author jaychoi
+ * Handles parsing the exponentiate or "exponentiate function".
+ * 
+ * @author Linkimals
  *
  */
-public class ExponentiateAnalyzer {
+public class ExponentiateAnalyzer implements SemanticAnalyzerInterface {
+	
+	private static ExponentiateAnalyzer singleton;
 
-	/**
-	 * 
-	 */
-	public ExponentiateAnalyzer() {
-		// TODO Auto-generated constructor stub
+	public static ExponentiateAnalyzer getInstance() {
+		if (singleton == null) {
+			singleton = new ExponentiateAnalyzer();
+		}
+		return singleton;
 	}
 
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		// Remove the division token
+		tokens.pop();
+			
+		// the parameters are the next tokens on the stack.
+		// But, they need to be processed	
+		ExpressionTreeNode right = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		ExpressionTreeNode left = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+			
+		return new Exponentiate(left, right);
+	}
 }
