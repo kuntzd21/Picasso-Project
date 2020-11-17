4f7170f96d4abfa6d9dfa099f113e7014ed32435
Nick Steinert <nicholassteinert@nicholass-mbp.ad.wlu.edu>
2020-11-17 15:56:30 -0500

    made atan silentley return 0 if needed

--- src/picasso/parser/language/expressions/ATangent.java
+++ src/picasso/parser/language/expressions/ATangent.java
@@ -29,9 +29,27 @@ public class ATangent extends UnaryFunction {
 	@Override
 	public RGBColor evaluate(double x, double y) {
 		RGBColor result = param.evaluate(x, y);
-		double red = Math.atan(result.getRed());
-		double green = Math.atan(result.getGreen());
-		double blue = Math.atan(result.getBlue());
+		double red;
+		double green;
+		double blue;
+		
+		if (Math.sin(result.getRed()) == 0) {
+			red = 0;}
+		else {
+			red = Math.atan(result.getRed());
+		}
+				
+		if (Math.sin(result.getGreen()) == 0) {
+			green = 0;}
+		else {
+			green = Math.atan(result.getGreen());
+		}
+
+		if (Math.sin(result.getBlue()) == 0) {
+			blue = 0;}
+		else {
+			blue = Math.atan(result.getBlue());
+		}
 
 		return new RGBColor(red, green, blue);
 	}
