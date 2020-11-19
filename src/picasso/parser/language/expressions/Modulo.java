e1c2e7d71d4b402211cf90c3c3653daa638ddc1c
Jay Choi <choij21@mail.wlu.edu>
2020-11-19 15:30:27 -0500

    Added modulo by 0 handling.

--- src/picasso/parser/language/expressions/Modulo.java
+++ src/picasso/parser/language/expressions/Modulo.java
@@ -26,9 +26,28 @@ public class Modulo extends BinaryOperator {
 		RGBColor result = left.evaluate(x, y);
 		RGBColor result2 = right.evaluate(x, y);
 		
-		double red = result.getRed() % result2.getRed();
-		double green = result.getGreen() % result2.getGreen();
-		double blue = result.getBlue() % result2.getBlue();
+		double red;
+		double green;
+		double blue;
+		
+		if (result2.getRed() == 0) {
+			red = 0;
+		}
+		else {
+			red = result.getRed()%result2.getRed();
+		}
+		if (result2.getGreen() == 0) {
+			green = 0;
+		}
+		else {
+			green = result.getGreen()%result2.getGreen();
+		}
+		if (result2.getBlue() == 0) {
+			blue = 0;
+		}
+		else {
+			blue = result.getBlue()%result2.getBlue();
+		}
 
 		return new RGBColor(red, green, blue);
 	}
